5ce4a0185f5cbe26e2d22f86332c6944b855beee
kelliechan1 <kelliechan30@gmail.com>
2018-07-25 15:03:42 -0700

    slideshow

--- edits.js
+++ edits.js
@@ -1,6 +1,9 @@
 window.onscroll = function() {myFunction()};
+
 var navbar = document.getElementById("navbar");
+
 var sticky = navbar.offsetTop;
+
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
